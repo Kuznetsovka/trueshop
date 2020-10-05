6779e211e4a739047b5195edee96c383c6947bb3
Kirill Kuznetsov <kuznechik.86@mail.ru>
2020-10-05 23:07:35 +0300

    Initial commit

---
+++ src/main/java/com/kuznetsovka/trueshop/TrueshopApplication.java
@@ -0,0 +1,13 @@
+package com.kuznetsovka.trueshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrueshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrueshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuznetsovka/trueshop/TrueshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuznetsovka.trueshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrueshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
